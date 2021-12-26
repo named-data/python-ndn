ec3373becf2adcb8d1b73930a2212a14fbcfb488
GlassyYang <glassyyang@outlook.com>
2021-12-26 22:05:40 +0800

    style: change face_test.py test cases style

--- tests/face_test.py
+++ tests/face_test.py
@@ -22,68 +22,69 @@ from ndn.transport.stream_face import TcpFace, UnixFace
 from ndn.transport.udp_face import UdpFace
 
 
-def test():
-    url = 'unix:///tmp/nfd.sock'
-    face = default_face(url)
-    assert isinstance(face, UnixFace)
-    assert face.path == '/tmp/nfd.sock'
+class TestFace:
+    def test(self):
+        url = 'unix:///tmp/nfd.sock'
+        face = default_face(url)
+        assert isinstance(face, UnixFace)
+        assert face.path == '/tmp/nfd.sock'
 
-    url = 'tcp://localhost'
-    face = default_face(url)
-    assert isinstance(face, TcpFace)
-    assert face.host == 'localhost'
-    assert face.port == 6363
+        url = 'tcp://localhost'
+        face = default_face(url)
+        assert isinstance(face, TcpFace)
+        assert face.host == 'localhost'
+        assert face.port == 6363
 
-    url = 'tcp4://localhost:6364'
-    face = default_face(url)
-    assert isinstance(face, TcpFace)
-    assert face.host == 'localhost'
-    assert face.port == 6364
+        url = 'tcp4://localhost:6364'
+        face = default_face(url)
+        assert isinstance(face, TcpFace)
+        assert face.host == 'localhost'
+        assert face.port == 6364
 
-    url = 'tcp6://localhost'
-    face = default_face(url)
-    assert isinstance(face, TcpFace)
-    assert face.host == 'localhost'
-    assert face.port == 6363
+        url = 'tcp6://localhost'
+        face = default_face(url)
+        assert isinstance(face, TcpFace)
+        assert face.host == 'localhost'
+        assert face.port == 6363
 
-    url = 'udp://localhost'
-    face = default_face(url)
-    assert isinstance(face, UdpFace)
-    assert face.host == 'localhost'
-    assert face.port == 6363
+        url = 'udp://localhost'
+        face = default_face(url)
+        assert isinstance(face, UdpFace)
+        assert face.host == 'localhost'
+        assert face.port == 6363
 
-    url = 'udp4://localhost:6364'
-    face = default_face(url)
-    assert isinstance(face, UdpFace)
-    assert face.host == 'localhost'
-    assert face.port == 6364
+        url = 'udp4://localhost:6364'
+        face = default_face(url)
+        assert isinstance(face, UdpFace)
+        assert face.host == 'localhost'
+        assert face.port == 6364
 
-    url = 'udp6://localhost'
-    face = default_face(url)
-    assert isinstance(face, UdpFace)
-    assert face.host == 'localhost'
-    assert face.port == 6363
+        url = 'udp6://localhost'
+        face = default_face(url)
+        assert isinstance(face, UdpFace)
+        assert face.host == 'localhost'
+        assert face.port == 6363
 
-    url = 'udp6://[::1]:6465'
-    face = default_face(url)
-    assert isinstance(face, UdpFace)
-    assert face.host == '::1'
-    assert face.port == 6465
+        url = 'udp6://[::1]:6465'
+        face = default_face(url)
+        assert isinstance(face, UdpFace)
+        assert face.host == '::1'
+        assert face.port == 6465
 
 
-def test_connection():
-    url = 'udp://localhost'
-    face = default_face(url)
-    asyncio.run(face.open())
+    def test_connection(self):
+        url = 'udp://localhost'
+        face = default_face(url)
+        asyncio.run(face.open())
 
-    url = 'udp4://localhost:6364'
-    face = default_face(url)
-    asyncio.run(face.open())
+        url = 'udp4://localhost:6364'
+        face = default_face(url)
+        asyncio.run(face.open())
 
-    url = 'udp6://localhost'
-    face = default_face(url)
-    asyncio.run(face.open())
+        url = 'udp6://localhost'
+        face = default_face(url)
+        asyncio.run(face.open())
 
-    url = 'udp6://[::1]:6465'
-    face = default_face(url)
-    asyncio.run(face.open())
+        url = 'udp6://[::1]:6465'
+        face = default_face(url)
+        asyncio.run(face.open())
